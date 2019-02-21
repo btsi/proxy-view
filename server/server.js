28c57cf3140c072609e14a2219d83cc85191f334
MikeJDev <ukmjanes@gmail.com>
2019-02-21 12:04:54 -0600

    added server file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000;
+
+
+app.use("/", express.static("dist"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(port)
