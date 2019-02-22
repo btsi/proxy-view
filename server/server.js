a90ab1caa79937eaf90c44f44b70bbea2f24a118
MikeJDev <ukmjanes@gmail.com>
2019-02-22 09:32:05 -0600

    added css styling

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000;
 
-
 app.use("/", express.static("dist"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
